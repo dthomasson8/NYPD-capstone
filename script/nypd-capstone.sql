0b1f2a3b001392527d37f462eaf69b2637daf88b
Drew Thomasson <drew.thomasson@technologyadvice.com>
2022-12-03 14:07:45 -0600

    Data imported and started to write queries

---
+++ script/nypd-capstone.sql
@@ -0,0 +1,15 @@
+select *
+from arrests;
+
+/* Number of arrests on each day, in descending order. September 29 had the most arrests. */ 
+select arrestdate, count(arrestdate)
+from arrests
+group by 1
+order by 2 desc;
+
+
+/* count of arrests by sex: male (115,970), female (24,594)*/
+Select sex, count(sex)
+from arrests
+group by 1
+order by 2 desc;
