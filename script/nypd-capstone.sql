e1f2a077c372d5b8e2d66f82f8813398613f1e93
Drew Thomasson <drew.thomasson@technologyadvice.com>
2022-12-08 21:11:13 -0600

    exploring different joins

--- script/nypd-capstone.sql
+++ script/nypd-capstone.sql
@@ -16,10 +16,16 @@ group by 1
 order by 2 desc;
 
 
-/*  */
+/* count of the types of calls for service */
 select typedesc, count(typedesc)
 from calls
 group by 1
 order by 2 desc
 
 
+/*  */
+select arrestkey, eventid, arrestdate, race, sex, offensedesc, a.latitude, a.longitude
+from arrests as a
+full join calls as c
+on a.arrestkey = c.eventid
+group by 1,2,3,4,5,6,7,8
